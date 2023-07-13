21dc77c79614db6c0ea722a00b316000ffa3d554
Claus Haas <claus.haas@me.com>
2023-07-13 10:10:08 -0300

    useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,46 @@
-import Header from './components/Header';
+import { useState, useEffect } from 'react';
+import { fetchCoordinates } from './services';
+
+type Coordinates = {
+  latitude: number;
+  longitude: number;
+};
 
 function App() {
+  const [coordinates, setCoordinates] = useState<Coordinates | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await fetchCoordinates();
+      setCoordinates({ latitude: data.latitude, longitude: data.longitude });
+      setLoading(false);
+    }
+    fetchData();
+
+    const intervalId = setInterval(() => {
+      fetchData();
+    }, 3000);
+
+    return () => { // Essa é a função cleanup
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
   return (
-    <div>
-      <Header />
-      <p>Hello World!</p>
-    </div>
+    <>
+      <h1>International Space Station Location Tracker</h1>
+      {coordinates && (
+        <>
+          <h2>{`Latitude: ${coordinates.latitude}`}</h2>
+          <h2>{`Longitude: ${coordinates.longitude}`}</h2>
+        </>
+      )}
+    </>
   );
 }
 
