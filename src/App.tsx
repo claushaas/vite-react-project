fe27d83552c5e0b9f6b1d47aceaecfad8cd7af1c
Claus Haas <claus.haas@me.com>
2023-07-13 07:39:59 -0300

    outlet

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,20 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Coffee from './pages/Coffee';
 import CoffeeList from './pages/CoffeeList';
+import Layout from './components/Layout';
 import NavBar from './components/Nav-bar';
+import NotFound from './pages/Not-found';
 
 function App() {
   return (
-    <>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={ <Home /> } />
-        <Route path="/coffees" element={ <CoffeeList /> } />
-        <Route path="/coffees/:coffee" element={ <Coffee /> } />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={ <Layout /> }>
+        <Route index element={ <Home /> } />
+        <Route path="coffees" element={ <CoffeeList /> } />
+        <Route path="coffees/:coffee" element={ <Coffee /> } />
+      </Route>
+      <Route path="/*" element={ <NotFound /> } />
+    </Routes>
   );
 }
 
